211bfdc76104c87210a3420e1a51886fb3fc6993
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2019-06-23 00:30:07 +0300

    c

--- js/common.js
+++ js/common.js
@@ -7,6 +7,10 @@ function Slider(button) {
     menu = document.querySelector('.my-sidemenu'),
     menuLinks = menu.querySelectorAll('li');
 
+  let width = 400;
+  if (window.innerWidth <= 480) {
+    width = 320
+  }
   this.init = function () {
     window.addEventListener('click', (e) => {
       if (e.target == this.hamburger) {
@@ -21,17 +25,19 @@ function Slider(button) {
   }
   this.open = function () {
     this.hamburger.classList.add('is-active');
-    content.setAttribute("style", "transform: translateX(-400px); ");
+    content.setAttribute("style", `transform: translateX(-${width}px);`);
     overlay.setAttribute("style", "background-color: rgba(0, 0, 0, 0.65); z-index: 9999; ");
+    document.documentElement.setAttribute("style", "overflow: hidden");
   }
   this.close = function () {
     this.hamburger.classList.remove('is-active');
     content.setAttribute("style", "");
+    document.documentElement.setAttribute("style", "");
     overlay.style.backgroundColor = 'transparent'
     setTimeout(function () { overlay.setAttribute("style", "") }, 400);
   }
 }
-
+// console.log(body)
 let myMenu = new Slider('.hamburger')
 
 myMenu.init()
